1a5dece9317cef8ca1497984f869ec2b09f356c4
ConsoleDotLog-const <cliff.ahleong@gmail.com>
2021-01-30 16:33:51 +1300

    react router, icons, styled components added, nav bar started with globalstyle component added

---
+++ src/comps/Navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Nav,NavLink, NavIcon, Bars } from './navBarElements'
+
+const Navbar = () => {
+    return (
+        <div>
+            <Nav>
+                <NavLink to ='/'> CAFE</NavLink>
+                <NavIcon>
+                    <p>Menu</p>
+                    <Bars/>
+                </NavIcon>
+            </Nav>
+        </div>
+    )
+}
+
+export default Navbar
